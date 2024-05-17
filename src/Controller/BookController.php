012be8625d7a7ac10e253c811f3aa395837e371c
Miguel Segura <miguel.segura@outlook.fr>
2024-05-17 12:10:12 +0000

    bookController added #1

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BookController extends AbstractController
 {
-    #[Route('/book', name: 'app_book')]
-    public function index(): Response
+    #[Route('/book', name: 'book_index')]
+    public function index(BookRepository $bookRepo): Response
     {
         return $this->render('book/index.html.twig', [
+            'books' => $bookRepo->findAll(),
             'controller_name' => 'BookController',
         ]);
     }
