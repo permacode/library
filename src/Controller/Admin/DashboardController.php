7c797a160088450752236e5d4f74362c6c485eba
permacode y <ryudu26@hotmail.fr>
2024-06-04 08:02:06 +0000

    adding link in regiter page

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -33,7 +33,7 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'homepage');
+        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'app_book_index');
         yield MenuItem::linkToCrud('Books', 'fas fa-book', Book::class);
         yield MenuItem::linkToCrud('Users', 'fas fa-person', User::class);
         yield MenuItem::linkToCrud('Admins', 'fas fa-key', Admin::class);
